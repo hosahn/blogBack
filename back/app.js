fff6737a49a4e94a6dacdba0800074ff9f3b74a4
Hosan_Lee <hosahn13@gmail.com>
2022-07-19 01:38:00 +0900

    Fixed Port

--- back/app.js
+++ back/app.js
@@ -52,6 +52,6 @@ app.post("/contact", (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("App Started in Port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("App Started");
 });
